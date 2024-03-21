d5130228330bb4e6577ec619a398c4d844fd6759
Crystian Lefundes <crystian.lf@gmail.com>
2024-03-21 20:02:48 -0300

    feat: Cria servico de cliente

---
+++ clienteService/src/main/java/br/com/cryslefundes/javendas/clienteservice/ClienteServiceApplication.java
@@ -0,0 +1,18 @@
+package br.com.cryslefundes.javendas.clienteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@RefreshScope
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
+public class ClienteServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClienteServiceApplication.class, args);
+    }
+
+}
---
+++ clienteService/src/test/java/br/com/cryslefundes/javendas/clienteservice/controller/ClienteControllerTest.java
@@ -0,0 +1,7 @@
+package br.com.cryslefundes.javendas.clienteservice.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClienteControllerTest {
+
+}
