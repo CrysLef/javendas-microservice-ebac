646a8a3aa52ce185c45fc23d8965a0d2420b59bf
Crystian Lefundes <crystian.lf@gmail.com>
2024-03-21 19:45:48 -0300

    feat: Cria servico de servidor de configuracao de microsservicos

---
+++ configServer/src/test/java/br/com/cryslefundes/javendas/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cryslefundes.javendas.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
